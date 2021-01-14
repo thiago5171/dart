732dc2a0ac948e7f4096647766714ee28b614011
thiago5171 <tgazaroli@gmail.com>
2021-01-14 00:31:21 -0200

    Create cmds2.dart

---
+++ cmds2.dart
@@ -0,0 +1,10 @@
+// fazendo importaçoes de bibliotecas
+import 'dart:math';
+
+main() {
+
+ // o comando 'pow(numero,expoente);' serve para elevar um numero a um determinado expoente
+ int n= pow(5,2);
+ print(" 5 elevado a 2 é igual a $n");
+  // usar o "/n " durante um comando ira quebrar a linha
+}
