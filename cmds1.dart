a64a91e09dd40711aa220707535e7a0855336246
thiago5171 <tgazaroli@gmail.com>
2021-01-14 00:31:25 -0200

    Create cmds1.dart

---
+++ cmds1.dart
@@ -0,0 +1,33 @@
+main(){
+  /*int conta = 517;
+  String nomedeusuario = "thiago";
+  double saldo = 50;
+
+
+
+  // ou 
+
+  int conta;
+  String nomedeusuario;
+  double saldo;
+
+  conta = 517;
+  nomedeusuario = "thiago";
+  saldo = 50;*/
+
+
+ // usando print
+
+  int conta = 517;
+  String nomedeusuario = "thiago";
+  double saldo = 50;
+  
+  print("ola mundo");
+  print(nomedeusuario);
+  // usando o $  e colado o nome da variavel, nos invocamos o valor  atribuido na variavel 
+  //quando estiver dentros das aspas
+  print("meu nome é: $nomedeusuario");
+  print("meu saldo é: $saldo");
+  print("minha conta é: $conta ");
+
+}
