05f0fc67a50e19ec80e106b89abd092e38fd24ad
thiago5171 <tgazaroli@gmail.com>
2021-01-16 20:17:00 -0200

    Update ex001.dart

--- exercicios/ex001.dart
+++ exercicios/ex001.dart
@@ -1,3 +1,11 @@
- //SOMAR DOIS VALORES INTEIROS E EXIBIR O RESULTADO
- import 'dart:io';
- 
+import 'dart:io';
+main() {
+    double n1;
+    double n2;
+    print("digite um valor: ");
+    n1= stdin.readByteSync(); 
+    print("digite outro valor: ");
+    n2= stdin.readByteSync(); 
+  var soma = n1+n2;  
+  print("a soma de $n1 com $n2 é igual a $soma");
+}
