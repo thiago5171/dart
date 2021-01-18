f80733cb37278d569f025cadaf2f0fe27f336638
thiago5171 <tgazaroli@gmail.com>
2021-01-18 13:03:43 -0200

    Create ex006.dart

---
+++ exercicios/ex006.dart
@@ -0,0 +1,40 @@
+//Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada
+import 'dart:io';
+main() {
+int n, tabuada ;
+print("Digite um numero para ver sua tabuatda: ");
+n = int.parse(stdin.readLineSync());
+//1
+tabuada = n* 1 ;
+print("\n$n X 1 = $tabuada");
+//2
+tabuada = n* 2 ;
+print("$n X 2 = $tabuada");
+//3
+tabuada = n* 3 ;
+print("$n X 3 = $tabuada");
+//4
+tabuada = n* 4 ;
+print("$n X 4 = $tabuada");
+//5
+tabuada = n* 5 ;
+print("$n X 5 = $tabuada");
+//6
+tabuada = n* 6 ;
+print("$n X 6 = $tabuada");
+//7
+tabuada = n* 7 ;
+print("$n X 7 = $tabuada");
+//8
+tabuada = n* 8 ;
+print("$n X 8 = $tabuada");
+//9
+tabuada = n* 9 ;
+print("$n X 9 = $tabuada");
+//10
+tabuada = n* 10 ;
+print("$n X 10 = $tabuada");
+
+
+
+}
